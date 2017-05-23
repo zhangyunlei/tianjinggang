e454e601e75a0c5372e9a441bb79bfce53a6e6a8
zhangyunlei <18530900340@163.com>
2017-05-23 14:48:37 +0800

    gj

--- js/index.js
+++ js/index.js
@@ -1,49 +1,5 @@
 $(function () {
-
-    $(".slideTxtBox").slide({
-        trigger: "click"
-    });
-
-
-
-    $("#tabTitle li").click(function () {
-        $('.conR').addClass('unfold');
-        $(this).addClass("on").siblings().removeClass("on");
-        var i = $(this).index();
-        $('#tabContent>ul').eq(i).addClass("on").siblings().removeClass("on");
-        $('#btnCtrl').show();
-    });
-
-    $('#tabFold').on('click', function () {
-        closeMenu();
-    });
-    function closeMenu() {
-        $('.conR').removeClass('unfold');
-        $('#tabContent>ul').removeClass("on");
-        $("#tabTitle li").removeClass("on");
-        $('#btnCtrl').hide();
-    }
-
-    $('#tabLock').on('click', function () {
-        $(this).toggleClass('on');
-        if ($(this).hasClass('on')) {
-            $('#tabFold').off('click');
-        } else {
-            $('#tabFold').on('click', function () {
-                closeMenu();
-            });
-        }
-    });
-
-//        导航菜单
-    $('.user-nav-bar').click(function(){
-        $(this).next().toggle();
-    });
-    //    左侧导航高度自适应，最小高度一屏
-    $(window).resize(function(){
-        location.replace(location);
-    });
-    var h=$('.con').height();
+    var h=$('.conR').height()+40;
+    console.log(h);
     $('.conL').height(h);
-
 });
